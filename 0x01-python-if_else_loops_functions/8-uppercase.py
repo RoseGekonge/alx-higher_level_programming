b9a758a3a5439a5491fa68dfd383d7dab0e502bf
RoseGekonge <rgekonge2001@gmail.com>
2023-08-09 00:06:06 +0300

    sentence to upper

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
+    upper_str = ""
     for i in range(len(str)):
         if ord(str[i]) >= 97 and ord(str[i]) < 123:
-            print("{}".format(chr(ord(str[i]) - 32)),end="")
+            upper_str += chr(ord(str[i]) - 32)
         else:
-            print("{}".format(str[i]), end="")
-    print("\n",end="")
+            upper_str += str[i]
+    print("{}".format(upper_str))
