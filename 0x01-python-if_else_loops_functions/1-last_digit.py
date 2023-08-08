b74f1294e5981aff516e41d75de96d44ec09f4a1
RoseGekonge <rgekonge2001@gmail.com>
2023-08-08 16:57:17 +0300

    hexadecimal and last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,16 +4,20 @@ number = random.randint(-10000, 10000)
 if number < 0:
     num = number * -1
     if num % 10 > 5:
-        print("Last digit of {} is {} and is greater than 5". format(number, -1*(num % 10)))
+        print("Last digit of {} is {}". format(number, -1*(num % 10)), end="")
+        print(" and is greater than 5")
     elif num % 10 == 0:
         print("Last digit of {} is {} and is 0". format(number, -1*(num % 10)))
     else:
-        print("Last digit of {} is {} and is less than 6 and not 0". format(number, -1*(num % 10)))
+        print("Last digit of {} is {}". format(number, -1*(num % 10)), end="")
+        print(" and is less than 6 and not 0")
 
 else:
     if number % 10 > 5:
-        print("Last digit of {} is {} and is greater that 5". format(number, number % 10))
+        print("Last digit of {} is {}". format(number, number % 10))
+        print(" and is greater that 5")
     elif number % 10 == 0:
         print("Last digit of {} is {} and is 0". format(number, number % 10))
     else:
-        print("Last digit of {} is {} and is less than 6 and not 0". format(number, number % 10))
+        print("Last digit of {} is {}". format(number, number % 10), end="")
+        print(" and is less than 6 and not 0")
