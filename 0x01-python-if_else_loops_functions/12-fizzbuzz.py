94a135f132d92b892f0545ed9caa00c203441dcc
RoseGekonge <rgekonge2001@gmail.com>
2023-08-08 21:46:08 +0300

    Fizzbuzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(0, 100):
+        if (i + 1) % 3 == 0 and (i + 1) % 5 == 0:
+            print("FizzBuzz", end=" ")
+        elif (i + 1) % 3 == 0:
+            print("Fizz", end=" ")
+        elif (i + 1) % 5 == 0:
+            print("Buzz", end=" ")
+        else:
+            print("{}".format(i + 1), end=" ")
+    print("\n", end="")
