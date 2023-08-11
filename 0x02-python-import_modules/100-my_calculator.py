fc521b7ead8733221b1db48dd103ce32956deeb5
RoseGekonge <rgekonge2001@gmail.com>
2023-08-11 16:29:50 +0300

    argv calculation

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
-    import sys
+    from sys import argv
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit 1
+        exit (1)
     else:
-        if argv[2] == '+':
-            print("{}".format(add(argv[1], argv[3])))
-        elif argv[2] == '-':
-            print("{}".format(sub(argv[1], argv[3])))
-        elif argv[2] == '*':
-            print("{}".format(mul(argv[1], argv[3])))
-        elif argv[2] == '/':
-            print("{}".format(div(argv[1], argv[3])))
+        if argv[2] == "+":
+            print("{}".format(add(int(argv[1]), int(argv[3]))))
+        elif argv[2] == "-":
+            print("{}".format(sub(int(argv[1]), int(argv[3]))))
+        elif argv[2] == "*":
+            print("{}".format(mul(int(argv[1]), int(argv[3]))))
+        elif argv[2] == "/":
+            print("{}".format(div(int(argv[1]), int(argv[3]))))
         else:
             print("Unknown operator. Available operators: +, -, * and /")
-            exit 1
+            exit (1)
 
