f25f920c445b003f45324e9c30fe8e475faa3ce8
RoseGekonge <rgekonge2001@gmail.com>
2023-08-12 20:26:24 +0300

    print matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,4 +2,4 @@
 def print_matrix_integer(matrix=[[]]):
     for i in range(len(matrix)):
         for r in range(len(matrix[])):
-            print("{}".format(int(matrix[i][r])))
+            print("{:d}".format(matrix[i][r]))
