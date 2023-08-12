ebc503fcef4c3d761abddf2d065774a8e1765e9b
RoseGekonge <rgekonge2001@gmail.com>
2023-08-12 19:55:25 +0300

    remove c

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in
+    for i in range(len(matrix)):
+        for r in range(len(matrix[])):
+            print("{}".format(matrix[i][r]))
