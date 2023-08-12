e9bce2231d325bb59aa3b71967ccbb3610a4e5cd
RoseGekonge <rgekonge2001@gmail.com>
2023-08-13 01:52:50 +0300

    sum of tuple integers

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,14 +2,14 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     new_tuple = ()
     i = 0
-    while True:
-        if i == len(tuple_a):
-            new_tuple += tuple_b[i:len(tuple_b)]
-            break
-        elif i == len(tuple_b):
-            new_tuple += tuple_a[i:len(tuple_a)]
-            break
+    while i != 2:
+        if i >= len(tuple_a) and i >= len(tuple_b):
+            new_tuple = new_tuple + (0,)
+        elif i >= len(tuple_a):
+            new_tuple = new_tuple + (int(0 + int(tuple_b[i])),)
+        elif i >= len(tuple_b):
+            new_tuple = new_tuple + (int(int(tuple_a[i]) + 0),)
         else:
             new_tuple = new_tuple + (int(int(tuple_a[i]) + int(tuple_b[i])),)
-            i += 1
+        i += 1
     return new_tuple
