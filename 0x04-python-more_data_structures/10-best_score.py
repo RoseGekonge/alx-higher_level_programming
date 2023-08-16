29703d683ed3eb039ad2d8381a190c5d0258b89f
RoseGekonge <rgekonge2001@gmail.com>
2023-08-16 17:51:32 +0300

    element with largest value

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    a = 0
+    if a_dictionary == None:
+        return None
+    for i in a_dictionary:
+        if a == 0:
+            largest = i
+        else:
+            if a_dictionary.get(i) > a_dictionary.get(largest):
+                largest = i
+            else:
+                continue
+        a += 1
+    return largest
