3a66ecbe32b1f0ca718dd725b01472ccd211be01
RoseGekonge <rgekonge2001@gmail.com>
2023-08-29 22:42:46 +0300

    raising errors

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,7 +6,7 @@
 class Square:
     """attribute defination"""
     def __init__(self, size=0):
-            if isinstance(size, int) == True:
+            if isinstance(size, int) == False:
                 raise TypeError("size must be an integer")
             if size < 0:
                 raise ValueError("size must be >= 0")
