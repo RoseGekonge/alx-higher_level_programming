31644ae672e7cd098d0c8c2c1ade3f20d75e2553
RoseGekonge <rgekonge2001@gmail.com>
2023-08-29 23:42:28 +0300

    property and property setters

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""Square class"""
+
+
+class Square:
+    """attribute defination"""
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        return int(self.__size) ** 2
