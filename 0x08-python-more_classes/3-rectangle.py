c44fc9c458c21be8ec18c6d8e624fe1df21dd064
RoseGekonge <rgekonge2001@gmail.com>
2023-09-04 01:34:01 +0300

    adding class public methods of area and perimeter

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -41,4 +41,4 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return 0
         else:
-            return 2 * (self.__width + self.__height)
+            return (self.__width + self.__height + self.__width + self.__height)
