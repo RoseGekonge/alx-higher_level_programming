7f80f290d855e17e0c440389778c2f6631e3f585
RoseGekonge <rgekonge2001@gmail.com>
2023-09-05 01:47:58 +0300

    deleting an instance

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -50,4 +50,7 @@ class Rectangle:
         return arr
 
     def __repr__(self):
-        return f"hhhhhRectangle({self.__width}, {self.__height})"
+        return f"Rectangle({self.__width}, {self.__height})"
+
+    def __del__(self):
+        print("Bye rectangle...")
