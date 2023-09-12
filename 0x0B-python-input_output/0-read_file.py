ad23ba75a15ddadec9d174d149cbdb047e1d911b
RoseGekonge <rgekonge2001@gmail.com>
2023-09-12 21:39:19 +0300

    reading an external text file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,6 +3,7 @@
 
 
 def read_file(filename=""):
+    """function that reads into files"""
     with open(filename, 'r', encoding='utf-8') as file:
         for line in file:
             print(line, end='')
