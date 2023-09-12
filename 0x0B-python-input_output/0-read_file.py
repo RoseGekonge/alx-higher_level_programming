d35f25aa37b08a51ca6d42fe9b1440168b923bbc
RoseGekonge <rgekonge2001@gmail.com>
2023-09-12 21:37:17 +0300

    reading an external text file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,9 +3,6 @@
 
 
 def read_file(filename=""):
-    try:
-        with open(filename, 'r', encoding='utf-8') as file:
-            for line in file:
-                print(line, end='')
-    except FileNotFoundError:
-        print(f"File '{filename}' not found.")
+    with open(filename, 'r', encoding='utf-8') as file:
+        for line in file:
+            print(line, end='')
