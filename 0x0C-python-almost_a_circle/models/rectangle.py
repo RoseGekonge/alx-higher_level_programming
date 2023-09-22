cf24a9d1c73f4c8d867110dd66af403891e10f7b
RoseGekonge <rgekonge2001@gmail.com>
2023-09-23 00:30:06 +0300

    python almost a circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """rectangle class"""
 from models.base import Base
+import tkinter as TK
 
 
 class Rectangle(Base):
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """square class"""
-
-
 from models.rectangle import Rectangle
+
+
 class Square(Rectangle):
     """class with defined attributes and methods"""
     def __init__(self, size, x=0, y=0, id=None):
